237738308c32cc894d136dcb2ef66e4d7f05819f
dzh <dzh_11@qq.com>
2018-12-27 20:39:16 +0800

    add kafka topic

---
+++ tbcloud-lib-api/src/main/java/tbcloud/lib/api/msg/MsgMeta.java
@@ -0,0 +1,12 @@
+package tbcloud.lib.api.msg;
+
+/**
+ * @author dzh
+ * @date 2018-12-27 15:16
+ */
+public interface MsgMeta {
+
+    String Topic_User = "tbcUser";
+    String Topic_Node = "tbcNode";
+
+}
